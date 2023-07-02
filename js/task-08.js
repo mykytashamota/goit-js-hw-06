e292bacb02e2534d17f7bdd0b560dd230e9b9a4e
mykytashamota <nik.shamota@gmail.com>
2023-07-02 15:37:15 +0300

    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please enter your email and password");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+    form.reset();
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const widget = document.querySelector(".widget");
+const spanColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
